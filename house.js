6fcb4829119d0af1998ece2c09e3df644f83387c
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-01-18 23:00:01 +0100

    added properties to object

--- house.js
+++ house.js
@@ -3,15 +3,61 @@ const house = {
     windows: 12,
     floors: 2,
     houseSizeInSqm: 160,
-    rooms: ["bedroom 1", "bedroom 2", "bedroom 3", "livingroom", "kitchen", ],
     color: "grey",
+    hasSolarPanels: true,
+    rooms: [
+        {
+            roomType: "hallway",
+            name: null,
+            color: "blue",
+        },
+        {
+            roomType: "livingroom",
+            name: null,
+            color: "blue",
+        },
+        {
+            roomType: "kitchen",
+            name: null,
+            color: "grey",
+        },
+        {
+            roomType: "bedroom",
+            name: "parents bedroom",
+            color: "blue",
+        },
+        {
+            roomType: "bedroom",
+            name: "Leanders bedroom",
+            color: "blue",
+    
+        },
+        {
+            roomType: "bedroom",
+            name: "Leonoras bedroom",
+            color: "pink",
+            
+        },
+        {
+            roomType: "bathroom",
+            name: "upstairs bathroom",
+            color: "grey",
+            
+        },
+        {
+            roomType: "bathroom",
+            name: "downstairs bathroom",
+            color: "white",
+        },
+    ],
+    
     garage: {
-        type: "double",
+        type: "double", 
         numberOfCars: 2,
         isOpen: false,
         openCloseGarage: function () {
             house.garage.isOpen = !house.garage.isOpen;
         }
     },
-    hasSolarPanels: true,
+    
 }
